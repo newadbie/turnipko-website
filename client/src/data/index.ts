4003c2df1e77f5d2b58274391d075f2938e57bdb
DESKTOP-99QR1Q7\Adrian <bielecadriandeveloper@gmail.com>
2021-02-16 00:45:33 +0100

    !fix Menu

--- client/src/data/index.ts
+++ client/src/data/index.ts
@@ -2,9 +2,9 @@ import { NavItemProps } from '../types'
 
 export const navItems: Array<NavItemProps> = [
   { text: 'Home', to: '/' },
-  { text: 'Gallery', to: '/gallery' },
-  { text: 'About', to: '/about' },
-  { text: 'Pricing', to: '/pricing' },
+  { text: 'Gallery', to: '/gallery/' },
+  { text: 'About', to: '/about/' },
+  { text: 'Pricing', to: '/pricing/' },
   {
     text: 'Contact',
     to: process.env.GATSBY_HOMEURL + '/#contact' ?? 'http://localhost:8000'
